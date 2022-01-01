b4a3dc7686b0d8e7cb42ead026279010a5dce876
Bas950 <me@bas950.com>
2022-01-01 19:43:50 +0100

    feat(getFlag): add function

---
+++ src/functions/flag/getFlag.ts
@@ -0,0 +1,15 @@
+import languages from "../..";
+import findLanguage from "../findLanguage";
+
+/**
+ * Gets the flag of a language
+ * @param country The country, country code, language locale, ISO code or name to find the flag of
+ * @returns The flag object of the language, or `null` if it is not found
+ */
+export default function getFlag(country: string) {
+	const language =
+		languages.find(l => l.country.toLowerCase() === country.toLowerCase()) ??
+		languages.find(l => l.countryCode.toLowerCase() === country.toLowerCase()) ??
+		findLanguage(country);
+	return language?.flag ?? null;
+}
---
+++ src/tests/flag/getFlag.test.ts
@@ -0,0 +1,64 @@
+import { getFlag } from "../..";
+
+describe("getIds()", () => {
+	test("Getting the ids of a language given its locale", () => {
+		expect(getFlag("nl-nl")).toStrictEqual({
+			image: "https://crowdin.com/images/flags/nl.png",
+			emoji: "🇳🇱",
+			primaryColor: { hex: "#FF4F00", rgb: [255, 79, 0], cmyk: [0, 69, 100, 0], base10: 16731904 },
+			flagColors: [
+				{ hex: "#C8102E", rgb: [200, 16, 46], cmyk: [0, 100, 80, 5], base10: 13111342 },
+				{ hex: "#FFFFFF", rgb: [255, 255, 255], cmyk: [0, 0, 0, 0], base10: 16777215 },
+				{ hex: "#003DA5", rgb: [0, 61, 165], cmyk: [100, 76, 0, 9], base10: 15781 }
+			]
+		});
+	});
+
+	test("Getting the ids of a language given its ISO codes", () => {
+		expect(getFlag("pt")).toStrictEqual({
+			image: "https://crowdin.com/images/flags/pt-PT.png",
+			emoji: "🇵🇹",
+			primaryColor: { hex: "#FF0000", rgb: [255, 0, 0], cmyk: [0, 100, 100, 0], base10: 16711680 },
+			flagColors: [
+				{ hex: "#046A38", rgb: [4, 106, 56], cmyk: [85, 3, 91, 44], base10: 289336 },
+				{ hex: "#DA291C", rgb: [218, 41, 28], cmyk: [0, 95, 100, 0], base10: 14297372 },
+				{ hex: "#FFE900", rgb: [255, 233, 0], cmyk: [0, 3, 97, 0], base10: 16771328 },
+				{ hex: "#002D72", rgb: [0, 45, 114], cmyk: [100, 79, 0, 37], base10: 11634 },
+				{ hex: "#FFFFFF", rgb: [255, 255, 255], cmyk: [0, 0, 0, 0], base10: 16777215 },
+				{ hex: "#000000", rgb: [0, 0, 0], cmyk: [0, 0, 0, 100], base10: 0 }
+			]
+		});
+		expect(getFlag("nld")).toStrictEqual({
+			image: "https://crowdin.com/images/flags/nl.png",
+			emoji: "🇳🇱",
+			primaryColor: { hex: "#FF4F00", rgb: [255, 79, 0], cmyk: [0, 69, 100, 0], base10: 16731904 },
+			flagColors: [
+				{ hex: "#C8102E", rgb: [200, 16, 46], cmyk: [0, 100, 80, 5], base10: 13111342 },
+				{ hex: "#FFFFFF", rgb: [255, 255, 255], cmyk: [0, 0, 0, 0], base10: 16777215 },
+				{ hex: "#003DA5", rgb: [0, 61, 165], cmyk: [100, 76, 0, 9], base10: 15781 }
+			]
+		});
+		expect(getFlag("afb")).toStrictEqual({
+			image: "https://crowdin.com/images/flags/ar-BH.png",
+			emoji: "🇧🇭",
+			primaryColor: { hex: "#F21731", rgb: [242, 23, 49], cmyk: [0, 91, 80, 5], base10: 15865649 },
+			flagColors: [
+				{ hex: "#CE1126", rgb: [206, 17, 38], cmyk: [0, 95, 100, 0], base10: 13504806 },
+				{ hex: "#FFFFFF", rgb: [255, 255, 255], cmyk: [0, 0, 0, 0], base10: 16777215 }
+			]
+		});
+	});
+
+	test("Getting the ids of a language given its name", () => {
+		expect(getFlag("english")).toStrictEqual({
+			image: "https://crowdin.com/images/flags/en.png",
+			emoji: "🇺🇸",
+			primaryColor: { hex: "#0A3161", rgb: [10, 49, 97], cmyk: [90, 49, 0, 62], base10: 668001 },
+			flagColors: [
+				{ hex: "#B31942", rgb: [179, 25, 66], cmyk: [0, 100, 66, 13], base10: 11737410 },
+				{ hex: "#FFFFFF", rgb: [255, 255, 255], cmyk: [0, 0, 0, 0], base10: 16777215 },
+				{ hex: "#0A3161", rgb: [10, 49, 97], cmyk: [100, 68, 0, 54], base10: 668001 }
+			]
+		});
+	});
+});
